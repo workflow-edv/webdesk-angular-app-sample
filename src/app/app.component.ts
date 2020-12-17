d43698b9240d1a9a97513ee1ccfa7c5e2b372fc6
Adrian Vasquez <vasquez.adrian.sh@gmail.com>
2020-12-17 20:35:13 +0100

    WD-12147 First impl

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Http, LoginService } from '@webdesk/webdesk-tools-websdk';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
+interface UserLogin {
+  username: string;
+  password: string;
+  server: string;
+}
+
 export class AppComponent {
   title = 'webdesk-angular-sdkApp';
+  loginForm;
+  http = new Http();
+
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      server: '',
+      username: '',
+      password: '',
+    });
+  }
+
+  onSubmit(value: { [key: string]: any; }): void {
+    console.log(value);
+    this.loginForm.patchValue(value);
+  }
+
+
+  login(user: UserLogin): void {
+    const baseURL = user.server;
+    const login = new LoginService({ baseURL });
+    login.doLogin(user.username, user.password);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookingComponent } from './booking/booking.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
