ee7a545dfe7dd944a8bc5a20b0439842ba1ccf42
Adrian Vasquez <TsukugiXIII@gmail.com>
2021-02-25 14:45:30 +0100

    NTLM Support first implementation
    - TODO: ta/book doesnt work yet

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,6 +3,7 @@ import { Http, LoginService } from "@webdesk/webdesk-tools-websdk";
 import { Component, OnInit } from "@angular/core";
 import { FormBuilder } from "@angular/forms";
 import { Router } from "@angular/router";
+import { IHttpConfig } from "@webdesk/webdesk-tools-websdk/dist/tools/http/http";
 
 interface UserLogin {
   username: string;
@@ -33,22 +34,44 @@ export class LoginComponent implements OnInit {
 
   async onSubmit(value: UserLogin): Promise<void> {
     this.loginForm.patchValue(value);
-    await this.login(value);
+    await this.NTLMLogin(value);
     this.router.navigate(["booking"]);
   }
 
   async login(user: UserLogin): Promise<void> {
     const baseURL = user.server;
 
-    const reqConfig = {
-      baseURL, timeout: 20000
+    const reqConfig: IHttpConfig = {
+      baseURL,
+      requestConfig: {
+        withCredentials: true
+      }
     };
 
     const http = new Http(reqConfig);
     window.state.http = http;
 
+    http.getServerInfo();
     const login = new LoginService({ baseURL });
     await login.doLogin(user.username, user.password);
+
+  }
+
+  async NTLMLogin(user: UserLogin): Promise<void> {
+    const baseURL = user.server;
+
+    const reqConfig: IHttpConfig = {
+      baseURL,
+      requestConfig: {
+        withCredentials: true
+      }
+    };
+
+    const http = new Http(reqConfig);
+    window.state.http = http;
+
+    const response = await http.get("getLoginInfos.do?format=json");
+    console.log(response);
 
   }
 }
